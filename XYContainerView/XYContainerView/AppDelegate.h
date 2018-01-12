1358b7858df8e2c06ce037607e764b45282fd8f6
aKerdi <jiangshaohong@ffrj.net>
2018-01-12 14:24:32 +0800

    First created

---
+++ XYContainerView/XYContainerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYContainerView
+//
+//  Created by aKerdi on 2018/1/12.
+//  Copyright © 2018年 XXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYContainerView/XYContainerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYContainerView
+//
+//  Created by aKerdi on 2018/1/12.
+//  Copyright © 2018年 XXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
